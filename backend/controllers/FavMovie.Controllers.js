54c8acb84a261bdf163248adf6fb01ee3d71c8de
Ana Vera <apveay@gmail.com>
2023-06-25 21:27:40 -0600

    deleteFavMovie controller with auth

--- backend/controllers/FavMovie.Controllers.js
+++ backend/controllers/FavMovie.Controllers.js
@@ -2,24 +2,31 @@ import asyncHandler from "express-async-handler"
 import FavMovie from "../models/FavMovie.Model.js"
 
 const setFavMovie = asyncHandler( async (req,res)=>{
-
     const favMovie = await FavMovie.create({
         userId: req.user.id,
         movieId: req.body.movieId
     })
-
     res.status(200).json({favMovie})
 } )
 
 const getFavMovies = asyncHandler( async (req,res)=>{
-    console.log(`The user is: ${req.user.name}`)
-    console.log(`The user ID is: ${req.user.id}`)
     const favMovies = await FavMovie.find({ userId: req.user.id })
     if(favMovies.length===0) console.log("this is empty")
     res.status(200).json({favMovies})
 })
 
+const deleteFavMovie = asyncHandler( async(req,res)=>{
+    const favMovie = await FavMovie.findById(req.params.id)
+    if(!favMovie){
+        res.status(400)
+        throw new Error('Cannot remove from favorites an unexisting movie')
+    }
+    await favMovie.deleteOne()
+    res.status(200).json({id: req.params.id})
+})
+
 export {
     setFavMovie,
-    getFavMovies
+    getFavMovies,
+    deleteFavMovie
 }
--- backend/routes/FavMovie.Routes.js
+++ backend/routes/FavMovie.Routes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import protect from '../middleware/authMiddleware.js'
-import {setFavMovie, getFavMovies} from '../controllers/FavMovie.Controllers.js'
+import {setFavMovie, getFavMovies, deleteFavMovie} from '../controllers/FavMovie.Controllers.js'
 
 const router = express.Router()
 
 router.post('/', protect, setFavMovie)
 router.get('/', protect, getFavMovies)
-
+router.delete('/:id',protect,deleteFavMovie)
 
 export default router 
